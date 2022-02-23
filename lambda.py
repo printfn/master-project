ff885838b335e54884b4ff9bdb2e0950ecab73c7
printfn <printfn@users.noreply.github.com>
2022-02-24 07:31:24 +1300

    Remove unused code

--- lambda.py
+++ lambda.py
@@ -7,13 +7,6 @@ import shutil
 import subprocess
 import time
 
-HELLO_WORLD = """
-reuse [L42.is/AdamsTowel]
-Main=(
-  Debug(S"Hello world from 42")
-  )
-"""
-
 def run_l42_program(code):
     if os.path.isdir('/tmp/L42Project'):
         shutil.rmtree('/tmp/L42Project')
@@ -76,13 +69,3 @@ def lambda_handler(event, context):
         },
         'body': json.dumps(result),
     }
-
-def list_files(startpath):
-    print('Listing files in {}:'.format(startpath))
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * (4 * level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
