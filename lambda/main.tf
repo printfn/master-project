7df979883f51fbf23d2151130146181d127ef37c
printfn <printfn@users.noreply.github.com>
2022-02-26 22:21:47 +1300

    Fix version constraints

--- lambda/main.tf
+++ lambda/main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.2.0"
+      version = ">=4.2.0, <5.0.0"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.2.0"
+      version = ">=2.2.0, <3.0.0"
     }
   }
 
-  required_version = "~> 1.0"
+  required_version = ">=1.0, <2.0"
 }
 
 provider "aws" {
