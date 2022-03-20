57fc4d670f9029d321c2e1109c9f0b16ade5f08e
printfn <printfn@users.noreply.github.com>
2022-03-20 21:52:29 +1300

    Api stub

--- l42-controller/src/l42client/Server.java
+++ l42-controller/src/l42client/Server.java
@@ -3,6 +3,7 @@ package l42client;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -11,16 +12,56 @@ import java.nio.charset.StandardCharsets;
 class HealthHttpHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        var response = "It works!";
-        exchange.sendResponseHeaders(200, response.length());
-        var stream = exchange.getResponseBody();
-        stream.write(response.getBytes(StandardCharsets.UTF_8));
-        stream.close();
+        Server.respond(200, "It works!", exchange);
+    }
+}
+
+class ApiHttpHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        if (!exchange.getRequestMethod().equals("POST")) {
+            Server.respond(405, "Method Not Allowed", exchange);
+            return;
+        }
+        var requestBodyStream = exchange.getRequestBody();
+        var requestBody = new String(requestBodyStream.readAllBytes(), StandardCharsets.UTF_8);
+        requestBodyStream.close();
+        var requestJson = new JSONObject(requestBody);
+
+        var responseJson = Server.handleApi(requestJson);
+
+        Server.respond(200, responseJson.toString(), exchange);
     }
 }
 
 public class Server {
     HttpServer httpServer;
+
+    static void respond(int code, String response, HttpExchange exchange) {
+        try {
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            exchange.sendResponseHeaders(code, response.length());
+            var stream = exchange.getResponseBody();
+            stream.write(response.getBytes(StandardCharsets.UTF_8));
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    static JSONObject handleApi(JSONObject request) {
+        var code = request.getString("code");
+        System.err.println("Received code:\n" + code);
+
+        var response = new JSONObject();
+        response.put("ok", true);
+        response.put("stdout", "STDOUT HERE");
+        response.put("stderr", "STDERR HERE");
+        response.put("returncode", 0);
+        response.put("duration", 1);
+        return response;
+    }
 
     Server() {
         this(8000);
@@ -34,9 +75,13 @@ public class Server {
         try {
             httpServer = HttpServer.create(new InetSocketAddress(bind, port), 0);
             httpServer.createContext("/health", new HealthHttpHandler());
+            httpServer.createContext("/api", new ApiHttpHandler());
             httpServer.setExecutor(null);
             httpServer.start();
-            System.err.println("Listening on http://" + bind + ":" + port + "/");
+            var baseUrl = "http://" + bind + ":" + port + "/";
+            System.err.println("Listening on " + baseUrl);
+            System.err.println("Health: " + baseUrl + "health");
+            System.err.println("42 API: " + baseUrl + "api");
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
