d141d8fd1898cd4286e33c482b5ee601553174a7
printfn <printfn@users.noreply.github.com>
2022-03-26 08:38:25 +1300

    Make it easy to override port

--- l42-controller/src/l42client/Main.java
+++ l42-controller/src/l42client/Main.java
@@ -1,6 +1,7 @@
 package l42client;
 
 import java.nio.file.Path;
+import java.text.NumberFormat;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -8,17 +9,27 @@ public class Main {
 
     public static void main(String[] args) {
         var client = new L42Client(Path.of("/tmp/L42testing"));
-        var server = new Server(client, "0.0.0.0", 8000);
         Path projectLocation = null;
+        int port = 8000;
 
         switch (args.length) {
             case 0 -> System.err.println("Warning: no project directory specified");
-            case 1 -> projectLocation = Path.of(args[0]);
+            case 1 -> {
+                try {
+                    port = Integer.parseInt(args[0]);
+                    System.err.println("Using port " + port);
+                } catch (NumberFormatException e) {
+                    projectLocation = Path.of(args[0]);
+                    System.err.println("Using project location " + projectLocation.toAbsolutePath());
+                }
+            }
             default -> {
                 System.err.println("Error: too many CLI arguments");
                 return;
             }
         }
+
+        var server = new Server(client, "0.0.0.0", port);
 
         Scanner reader = new Scanner(System.in);
         while (true) {
--- l42-docker-controller/README.md
+++ l42-docker-controller/README.md
@@ -13,11 +13,28 @@ docker build --network=host --tag l42-docker-controller .
 ### Run
 
 ```bash
-docker run --publish 8000:8000 -it l42-docker-controller
+docker run --publish 80:80 -it l42-docker-controller
 ```
 
 ### Debug
 
 ```bash
-docker run --publish 8000:8000 -v "$(pwd)/../l42-examples:/opt/app/examples" -it l42-docker-controller /bin/sh
+docker run --publish 80:80 -v "$(pwd)/../l42-examples:/opt/app/examples" -it l42-docker-controller /bin/sh
+```
+
+### Upload to AWS
+
+```bash
+# put the following lines into ~/L42_exports.sh
+#   export AWS_PROFILE=...
+#   export region=...
+#   export account=...
+source ~/L42_exports.sh
+
+aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $account.dkr.ecr.$region.amazonaws.com/l42-docker-controller
+aws ecr create-repository --region $region --repository-name l42-docker-controller
+aws ecr batch-delete-image --region $region --repository-name l42-docker-controller --image-ids imageTag=latest
+docker tag l42-docker-controller:latest $account.dkr.ecr.$region.amazonaws.com/l42-docker-controller:latest
+docker push $account.dkr.ecr.$region.amazonaws.com/l42-docker-controller:latest
+aws ecr list-images --region $region --repository-name l42-docker-controller
 ```
