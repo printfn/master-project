af0919a310ec88d982d4facd6137c2015c6e470c
printfn <printfn@users.noreply.github.com>
2022-04-13 15:59:27 +0200

    Create correct lambda_layer.zip file

--- lambda/update-layer.sh
+++ lambda/update-layer.sh
@@ -3,20 +3,30 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 if [[ -f "L42PortableLinux.zip" ]]; then
-    echo "Deleting existing L42PortableLinux.zip..."
-    rm "L42PortableLinux.zip"
-fi
-
-# use the aria2 program if it is installed, which allows for much faster parallel downloading
-# install with e.g. `brew install aria2`
-if type aria2c &>/dev/null; then
-    aria2c -x10 "https://l42.is/L42PortableLinux.zip"
+    echo >&2 "Using existing L42PortableLinux.zip"
 else
-    curl -O "https://l42.is/L42PortableLinux.zip"
+    echo >&2 "Downloading L42PortableLinux.zip..."
+    # use the aria2 program if it is installed, which allows for much faster parallel downloading
+    # install with e.g. `brew install aria2`
+    if type aria2c &>/dev/null; then
+        aria2c -x10 "https://l42.is/L42PortableLinux.zip"
+    else
+        curl -O "https://l42.is/L42PortableLinux.zip"
+    fi
+    echo >&2 "... successfully downloaded L42PortableLinux.zip"
 fi
 
-# delete these two large unused files from inside the zip archive
-echo "Deleting large unused files from inside zip file..."
-zip -d L42PortableLinux.zip \
+cp L42PortableLinux.zip lambda_layer.zip
+
+echo >&2 "Deleting large unused files from inside zip file..."
+zip -d lambda_layer.zip \
     L42PortableLinux/L42Internals/libjfxwebkit.so \
-    L42PortableLinux/L42Internals/jdk-16/lib/src.zip
+    L42PortableLinux/L42Internals/jdk-16/lib/src.zip \
+    L42PortableLinux/L42Internals/L42.jar \
+    L42PortableLinux/L42Internals/L42_lib/\*
+
+echo >&2 "Adding l42-controller.jar..."
+zip -j lambda_layer.zip \
+    ../l42-controller/out/artifacts/l42_controller_jar/l42-controller.jar
+
+echo >&2 "Successfully created lambda_layer.zip"
