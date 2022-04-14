704d2d2955acbece90a82a2317ea631d2b19890e
printfn <printfn@users.noreply.github.com>
2022-04-14 10:30:59 +0200

    Use function url instead of api gateway

--- lambda/_outputs.tf
+++ lambda/_outputs.tf
@@ -1,12 +1,12 @@
 output "editor_url" {
   description = "url to launch L42 editor"
-  value       = "https://printfn.github.io/master-project/editor/?server=${urlencode(aws_apigatewayv2_stage.l42_lambda.invoke_url)}execute"
-}
-
-output "editor_url_2" {
-  description = "url to launch L42 editor"
   value = join("", [
     "https://printfn.github.io/master-project/editor/?server=",
-    urlencode(trimprefix("https://", aws_lambda_function_url.l42_lambda_url.function_url))
+    urlencode(
+      trimprefix(
+        trimsuffix(aws_lambda_function_url.l42_lambda_url.function_url, "/"),
+        "https://"
+      )
+    )
   ])
 }
