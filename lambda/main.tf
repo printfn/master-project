e828fc356965d9d394bed9d35d4d3a76f68a0196
printfn <printfn@users.noreply.github.com>
2022-04-14 17:25:45 +0200

    Test deployment to ecs fargate

--- lambda/main.tf
+++ lambda/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">=4.2.0, <5.0.0"
     }
-    archive = {
-      source  = "hashicorp/archive"
-      version = ">=2.2.0, <3.0.0"
-    }
   }
 
   required_version = ">=1.0, <2.0"
